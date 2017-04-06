db556572740b8bfa6d6f64696cf03c3e096dedb0
Matthieu Le <matthieu.leminh@gmail.com>
2017-04-05 18:28:26 -0700

    First commit

---
+++ gphmc.py
@@ -0,0 +1,155 @@
+"""GPHMC"""
+import numpy as np
+
+from gaussian_process_regression.covariance import Covariance
+from gaussian_process_regression.optimizer import LikelihoodOptimizer
+
+
+class NoExplorationError(Exception):
+    """Raised when the exploration stage has been skipped"""
+    pass
+
+
+class GPHMCSampler:
+
+    def __init__(self, dimension, covariance_class: Covariance, n_explo,
+                 epsilon, dynamics_length,
+                 likelihood_optimizer_class: LikelihoodOptimizer, list_obs,
+                 list_y, init_cov_param, noise, target_function):
+        """Init
+
+        :param dimension: int, the dimension of the parameter space
+        :param covariance_class: the class of the covariance matrix
+        :param n_explo: the number of exploratory space to refine the Gaussian
+         process
+        :param epsilon: the numerical parameter of the leapfrog scheme
+        :param dynamics_length: the length of the Hamiltonian dynamics
+        :param likelihood_optimizer_class: the class of the likelihood
+         optimizer
+        :param list_obs: the initial list of observation
+        :param list_y: the initial list of target function evaluation
+        :param init_cov_param: the initial parameters of the covariance
+         function
+        :param noise: the noise
+        :param target_function: the target function to be sampled (i.e. the
+         negative log lokelihood of the density probability of interest)
+        """
+
+        self.likelihood_optimizer = likelihood_optimizer_class(
+            covariance_class, list_obs, list_y, initial_guess=init_cov_param,
+            noise=noise
+        )
+        self.dimension = dimension
+        self.n_explo = n_explo
+        self.epsilon = epsilon
+        self.dynamics_length = dynamics_length
+        self.list_obs = list_obs
+        self.list_y = list_y
+        self.current_cov_param = init_cov_param
+        self.target_function = target_function
+        self.init_y = None
+        self.init_obs = None
+        self.gp = None
+
+        self._assert_dimension()
+
+    def _assert_dimension(self):
+
+        for obs, y in zip(self.list_obs, self.list_y):
+            assert len(y) == self.dimension
+            assert len(obs) == self.dimension
+
+    def _add_observation(self, obs_end):
+
+        self.list_obs.append(tuple(obs_end))
+        y_end = self.target_function(obs_end)
+        self.list_y.append(y_end)
+
+        return y_end
+
+    def _leapfrog_dynamics(self, type='sampling'):
+
+        obs = self.init_obs.copy()
+        momentum_start = np.random.randn(self.dimension)
+        momentum = momentum_start.copy()
+        momentum -= self.gp_grad(obs, type=type)*self.epsilon/2
+        for idx in range(self.dynamics_length):
+            obs += momentum*self.epsilon
+            if idx < self.dynamics_length-1:
+                momentum -= self.gp_grad(obs, type=type)*self.epsilon
+        momentum -= self.gp_grad(obs, type=type)*self.epsilon/2
+
+        result = {
+            'obs_1': self.init_obs,
+            'obs_2': obs,
+            'momentum_1': momentum_start,
+            'momentum_2': momentum
+        }
+
+        return result
+
+    def gp_grad(self, obs, type):
+
+        gp_grad = np.array([
+            self.gp.mean([tuple(obs)], derivative=True, i=i)
+            for i in range(self.dimension)
+        ])
+
+        if type == 'exploration':
+            sigma_grad = np.array([
+                self.gp.sigma([tuple(obs)], derivative=True, i=i)
+                for i in range(self.dimension)
+            ])
+            gp_grad -= sigma_grad
+
+        return gp_grad
+
+    def _fit_covariance_parameters(self):
+
+        res = self.likelihood_optimizer.maximum_likelihood()
+        self.current_cov_param = res.x
+
+    def _update_position(self, leapfrog_output, y_1, y_2):
+
+        kin_energy_1 = np.sum(
+            np.power(leapfrog_output['momentum_1'], 2)
+        )/2
+        kin_energy_2 = np.sum(
+            np.power(leapfrog_output['momentum_2'], 2)
+        ) / 2
+
+        if np.random.rand(1) < np.exp(y_1-y_2+kin_energy_1-kin_energy_2):
+            return leapfrog_output['obs_2'], y_2
+        else:
+            return leapfrog_output['obs_1'], y_1
+
+    def _update_current_sample(self, type):
+
+        leapfrog_output = self._leapfrog_dynamics(type=type)
+        y_end = self._add_observation(leapfrog_output['obs_2'])
+        self.init_obs, self.init_y = self._update_position(
+            leapfrog_output, self.init_y, y_end
+        )
+
+    def exploration(self):
+
+        self._fit_covariance_parameters()
+
+        self.init_y = np.min[self.list_y]
+        self.init_obs = self.list_obs[np.argmin[self.list_y]]
+        for _ in range(self.n_explo):
+            self._update_current_sample('exploration')
+            self._fit_covariance_parameters()
+
+        self.gp = self.likelihood_optimizer.instanciate_gp(
+            self.current_cov_param
+        )
+
+    def sample(self):
+
+        if not (self.init_obs and self.init_y and self.gp):
+            raise NoExplorationError
+
+        self._update_current_sample('sampling')
+
+        yield self.init_obs
---
+++ script/example_2d.py
@@ -0,0 +1,59 @@
+"""GPHMC 2D example"""
+import numpy as np
+
+from gphmc import GPHMCSampler
+from gaussian_process_regression.covariance import SquaredExponential
+from gaussian_process_regression.optimizer import SECovLikelihoodOptimizer
+
+
+def potential_energy(x):
+    """Density to estimate
+
+    :param x: input parameter
+    :return: value of the density at (x, y)
+    """
+
+    probability = np.exp(-(1-(x[0]**2/2+x[1]**2/0.5))**2/0.5)
+    potential_energy = -np.log(probability)
+    return potential_energy
+
+
+def get_observations(xmin, xmax, ymin, ymax, n):
+    """Get a list of random observation
+
+    :param xmin: minimum x value where to plot the density
+    :param xmax: maximum x value where to plot the density
+    :param ymin: minimum y value where to plot the density
+    :param ymax: maximum y value where to plot the density
+    :param n: number of observation
+    :return: two lists of the same length. One list of the observation points,
+     and one list of the evaluation of the density at the observation points
+    """
+
+    list_observations = []
+    list_y = []
+    for _ in range(n):
+        i = np.random.uniform(low=xmin, high=xmax)
+        j = np.random.uniform(low=ymin, high=ymax)
+        list_observations.append((i, j))
+        list_y.append(potential_energy(i, j))
+
+    return list_observations, list_y
+
+
+def main():
+
+    list_obs, list_y = get_observations(
+        xmin=-3, xmax=3, ymin=-3, ymax=3, n=15
+    )
+    GPHMCSampler(dimension=2, covariance_class=SquaredExponential, n_explo=100,
+                 epsilon=0.05, dynamics_length=20,
+                 likelihood_optimizer_class=SECovLikelihoodOptimizer,
+                 list_obs=list_obs, list_y=list_y,
+                 init_cov_param=np.array([1, 1, 1]),
+                 noise=1e-6, target_function=potential_energy)
+
+
+if __name__ == '__name__':
+
+    main()
