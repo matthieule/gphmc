d0d5e57addf8689a29f9da477398e56064f0e3d1
Matthieu Le <matthieu.leminh@gmail.com>
2017-05-24 10:14:02 -0700

    Finalize the example

--- gphmc/gphmc.py
+++ gphmc/gphmc.py
@@ -1,5 +1,7 @@
 """GPHMC"""
 import numpy as np
+from copy import deepcopy
+from tqdm import tqdm
 
 from gaussian_process_regression.gaussian_process.covariance import Covariance
 from gaussian_process_regression.gaussian_process.optimizer import (
@@ -15,7 +17,6 @@ class NoExplorationError(Exception):
 class GPHMCSampler:
 
     def __init__(self, dimension, covariance_class: Covariance, n_explo,
-                 epsilon, dynamics_length,
                  likelihood_optimizer_class: LikelihoodOptimizer, list_obs,
                  list_y, init_cov_param, noise, target_function):
         """Init
@@ -24,8 +25,6 @@ class GPHMCSampler:
         :param covariance_class: the class of the covariance matrix
         :param n_explo: the number of exploratory space to refine the Gaussian
          process
-        :param epsilon: the numerical parameter of the leapfrog scheme
-        :param dynamics_length: the length of the Hamiltonian dynamics
         :param likelihood_optimizer_class: the class of the likelihood
          optimizer
         :param list_obs: the initial list of observation
@@ -34,124 +33,295 @@ class GPHMCSampler:
          function
         :param noise: the noise
         :param target_function: the target function to be sampled (i.e. the
-         negative log lokelihood of the density probability of interest)
+         negative log likelihood of the density probability of interest)
         """
 
-        self.likelihood_optimizer = likelihood_optimizer_class(
-            covariance_class, list_obs, list_y, initial_guess=init_cov_param,
-            noise=noise
-        )
         self.dimension = dimension
         self.n_explo = n_explo
-        self.epsilon = epsilon
-        self.dynamics_length = dynamics_length
-        self.list_obs = list_obs
-        self.list_y = list_y
-        self.current_cov_param = init_cov_param
+        self._list_obs = deepcopy(list_obs)
+        self._list_y = deepcopy(list_y)
+        self.current_cov_param = init_cov_param.copy()
         self.target_function = target_function
         self.init_y = None
         self.init_obs = None
         self.gp = None
 
+        # Note that we make use of the fact that the lists are passed by
+        # reference here. We instantiate the likelihood_optimizer_class
+        # with _list_y and _list_obs which will be updated as observations are
+        # added
+        self.likelihood_optimizer = likelihood_optimizer_class(
+            covariance_class, self._list_obs, self._list_y,
+            initial_guess=init_cov_param, noise=noise
+        )
+
         self._assert_dimension()
 
     def _assert_dimension(self):
+        """Assert that the input list have the correct length"""
 
-        for obs, y in zip(self.list_obs, self.list_y):
-            assert len(y) == self.dimension
+        assert len(self._list_y) == len(self._list_obs)
+        for obs, y in zip(self._list_obs, self._list_y):
             assert len(obs) == self.dimension
 
-    def _add_observation(self, obs_end):
+    def _fit_covariance_parameters(self, maxiter=1000, disp=True):
+        """Fit the parameters of the covariance function"""
 
-        self.list_obs.append(tuple(obs_end))
-        y_end = self.target_function(obs_end)
-        self.list_y.append(y_end)
+        res = self.likelihood_optimizer.maximum_likelihood(
+            maxiter=maxiter, disp=disp
+        )
+        self.current_cov_param = res.x
 
-        return y_end
+    def _potential_energy_grad(self, obs, type):
+        """Compute the gradient of the potential energy
 
-    def _leapfrog_dynamics(self, type='sampling'):
+        The potential energy is approximated with a Gaussian process. In the
+        sampling phase, it is the mean of the Gaussian process. In the
+        exploration phase, it is the mean minus the standard deviation.
 
-        obs = self.init_obs.copy()
-        momentum_start = np.random.randn(self.dimension)
+        :param obs: array of dimension self.dimension
+        :param type: 'sampling' or 'exploration'
+        :return: the gradient of the potential energy and the standard
+         deviation of the Gaussian process at obs
+        """
+
+        assert len(obs) == self.dimension
+        assert type in ['sampling', 'exploration']
+
+        potential_energy_grad = np.squeeze(np.array([
+            self.gp.mean([tuple(obs)], derivative=True, i=i)
+            for i in range(self.dimension)
+        ]))
+        sigma = np.squeeze(self.gp.sigma([tuple(obs)]))
+        std = np.sqrt(sigma)
+
+        # During the exploratory phase, the potential energy is the sum of the
+        # normal potential energy with the opposite of the standard deviation
+        # to explore space of high uncertainty
+        if type == 'exploration':
+            std_grad = np.squeeze(np.array([
+                self.gp.sigma([tuple(obs)], derivative=True, i=i)
+                for i in range(self.dimension)
+            ]))
+            std_grad /= 2*std
+            potential_energy_grad -= std_grad
+
+        return potential_energy_grad, std
+
+    def _hamiltonian_dynamics(self, type='sampling', epsilon=0.1, length=100,
+                              std_thr=3, momentum_std=1.0):
+        """Hamiltonian dynamics using the leapfrog numerical scheme
+
+        The momentum is drawn from a normal distribution
+
+        :param type: 'sampling' or 'exploration'
+        :param epsilon: time step of the Hamiltonian dynamics
+        :param length: length of the Hamiltonian dynamics
+        :param std_thr: threshold on the standard deviation when to stop the
+         dynamics
+        :param momentum_std: standard deviation of the Gaussian distribution
+         the momentum is drawn from
+        :return: dictionary with keys 'dynamics_end_obs' the point at the end
+         of the dynamics, 'momentum_1' the momentum at the beginning of the
+         dynamics, 'momentum_2' the momentum at the end of the dynamics, and
+         'momentum_std' the standard deviation of the Gaussian distribution the
+         momentum is drawn from
+        """
+
+        assert type in ['sampling', 'exploration']
+
+        obs = deepcopy(self.init_obs)
+        momentum_start = np.random.randn(self.dimension)*momentum_std
         momentum = momentum_start.copy()
-        momentum -= self.gp_grad(obs, type=type)*self.epsilon/2
-        for idx in range(self.dynamics_length):
-            obs += momentum*self.epsilon
-            if idx < self.dynamics_length-1:
-                momentum -= self.gp_grad(obs, type=type)*self.epsilon
-        momentum -= self.gp_grad(obs, type=type)*self.epsilon/2
 
+        # Half step for the momentum
+        pot_energy_grad, std = self._potential_energy_grad(obs, type=type)
+        momentum -= pot_energy_grad*epsilon/2
+
+        # Run the Hamiltonian dynamics
+        for idx in range(length):
+            obs += momentum*epsilon
+            # For the last step, skip the momentum full update
+            if idx < length-1:
+                pot_energy_grad, std = (
+                    self._potential_energy_grad(obs, type=type)
+                )
+                momentum -= pot_energy_grad*epsilon
+            # If the standard deviation is greater than std_thr, stop the
+            # dynamics
+            if type == 'exploration' and std > std_thr:
+                break
+            if type == 'sampling':
+                energy_pot = self.gp.mean([tuple(obs)])
+
+        # Half step for the momentum
+        pot_energy_grad, std = self._potential_energy_grad(obs, type=type)
+        momentum -= pot_energy_grad*epsilon/2
         result = {
-            'obs_1': self.init_obs,
-            'obs_2': obs,
+            'dynamics_end_obs': tuple(obs),
             'momentum_1': momentum_start,
-            'momentum_2': momentum
+            'momentum_2': momentum,
+            'momentum_std': momentum_std
         }
 
         return result
 
-    def gp_grad(self, obs, type):
+    def _update_current_sample(self, type, epsilon=0.1, length=100,
+                               momentum_std=1.0):
+        """Update the current state of the sampler with Hamiltonian dynamics
 
-        gp_grad = np.array([
-            self.gp.mean([tuple(obs)], derivative=True, i=i)
-            for i in range(self.dimension)
-        ])
+        Run the Hamiltonian dynamics with the proposed parameters and
+        update the init_obs and init_y of the sampler with a
+        Metropolis-Hastings acceptance criteria
 
-        if type == 'exploration':
-            sigma_grad = np.array([
-                self.gp.sigma([tuple(obs)], derivative=True, i=i)
-                for i in range(self.dimension)
-            ])
-            gp_grad -= sigma_grad
+        :param type: 'sampling' or 'exploration'
+        :param epsilon: time step of the Hamiltonian dynamics
+        :param length: length of the Hamiltonian dynamics
+        :param momentum_std: standard deviation of the Gaussian distribution
+         the momentum is drawn from
+        """
 
-        return gp_grad
+        assert type in ['sampling', 'exploration']
 
-    def _fit_covariance_parameters(self):
+        dynamics_output = self._hamiltonian_dynamics(
+            type=type, epsilon=epsilon, length=length,
+            momentum_std=momentum_std
+        )
+        y_end = self.add_observation(dynamics_output['dynamics_end_obs'])
 
-        res = self.likelihood_optimizer.maximum_likelihood()
-        self.current_cov_param = res.x
-
-    def _update_position(self, leapfrog_output, y_1, y_2):
-
-        kin_energy_1 = np.sum(
-            np.power(leapfrog_output['momentum_1'], 2)
-        )/2
-        kin_energy_2 = np.sum(
-            np.power(leapfrog_output['momentum_2'], 2)
-        ) / 2
-
-        if np.random.rand(1) < np.exp(y_1-y_2+kin_energy_1-kin_energy_2):
-            return leapfrog_output['obs_2'], y_2
-        else:
-            return leapfrog_output['obs_1'], y_1
-
-    def _update_current_sample(self, type):
-
-        leapfrog_output = self._leapfrog_dynamics(type=type)
-        y_end = self._add_observation(leapfrog_output['obs_2'])
-        self.init_obs, self.init_y = self._update_position(
-            leapfrog_output, self.init_y, y_end
+        self.init_obs, self.init_y = (
+            self._metropolis_hastings_acceptance_criteria(
+                dynamics_output, y_end
+            )
         )
 
-    def exploration(self):
+    def _metropolis_hastings_acceptance_criteria(self, dynamics_output, y_2):
+        """Metropolis-Hastings acceptance criteria
 
-        self._fit_covariance_parameters()
+        :param dynamics_output: Output of the hamiltonian dynamics: Dictionary
+         with keys 'dynamics_end_obs' the point at the end of the dynamics,
+         'momentum_1' the momentum at the beginning of the dynamics,
+         'momentum_2' the momentum at the end of the dynamics, and
+         'momentum_std' the standard deviation of the Gaussian distribution the
+         momentum is drawn from
+        :param y_2: value of the target function at the end of the dynamics
+        :return: tuple with the accepted observation and target function
+         evaluation
+        """
 
-        self.init_y = np.min[self.list_y]
-        self.init_obs = self.list_obs[np.argmin[self.list_y]]
-        for _ in range(self.n_explo):
-            self._update_current_sample('exploration')
-            self._fit_covariance_parameters()
+        kin_energy_1 = self.kinetic_energy(
+            dynamics_output['momentum_1'], dynamics_output['momentum_std']
+        )
+        kin_energy_2 = self.kinetic_energy(
+            dynamics_output['momentum_2'], dynamics_output['momentum_std']
+        )
+        metropolis_hastings_ratio = (
+            np.exp(self.init_y - y_2 + kin_energy_1 - kin_energy_2)
+        )
+        random_nbr = np.random.rand(1)[0]
+        if random_nbr < metropolis_hastings_ratio:
+            return dynamics_output['dynamics_end_obs'], y_2
+        else:
+            return self.init_obs, self.init_y
 
+    def add_observation(self, obs):
+        """Add observation & target function evaluation
+
+        :param obs: tupple with size dimension
+        :return: evaluation of the target function at obs
+        """
+
+        assert len(obs) == self.dimension
+
+        self._list_obs.append(tuple(obs))
+        y_end = self.target_function(obs)
+        self._list_y.append(y_end)
+
+        return y_end
+
+    def define_gp(self, maxiter=1000, disp=True):
+        """Instantiate the Gaussian process with the best covariance param"""
+
+        self._fit_covariance_parameters(maxiter=maxiter, disp=disp)
         self.gp = self.likelihood_optimizer.instanciate_gp(
             self.current_cov_param
         )
+        self.gp.covariance_matrix()
 
-    def sample(self):
+    def exploration(self, epsilon=0.1, length=100, gp_update_rate=4,
+                    momentum_std=1.0):
+        """Exploration phase
+
+        The new observations and target function evaluations are added to
+        _list_obs and _list_y
+
+        :param epsilon: time step of the Hamiltonian dynamics
+        :param length: length of the Hamiltonian dynamics
+        :param gp_update_rate: int, rate at wich the Gaussian process
+         covariance parameters are updated
+        :param momentum_std: standard deviation of the Gaussian distribution
+         the momentum is drawn from
+        """
+
+        self.define_gp(disp=False)
+        self.init_y = np.min(self._list_y)
+        self.init_obs = self._list_obs[np.argmin(self._list_y)]
+        for idx in tqdm(range(self.n_explo)):
+            self._update_current_sample(
+                'exploration', epsilon=epsilon, length=length,
+                momentum_std=momentum_std
+            )
+            if idx % gp_update_rate == 0:
+                self.define_gp(disp=False)
+
+    @staticmethod
+    def kinetic_energy(momentum, std):
+        """Kinetic energy
+
+        :param momentum: fictitious momentum
+        :param std: standard deviation of the Gaussian distribution the
+         momentum is drawn from
+        :return: kinetic energy corresponding to the momentum
+        """
+
+        energy = np.sum(np.power(momentum/std, 2))/2
+
+        return energy
+
+    @property
+    def list_y(self):
+        """List of target function evaluation"""
+
+        return deepcopy(self._list_y)
+
+    @property
+    def list_obs(self):
+        """List of observation where the target function has been evaluated"""
+
+        return deepcopy(self._list_obs)
+
+    def sample(self, epsilon=0.1, length=100, momentum_std=1.0):
+        """Sampling phase
+
+        This is a generator which will generate new samples according to the
+        state of the sampler and the input parameters. The new observations and
+        target function evaluations will be added to _list_obs and _list_y
+
+        :param epsilon: time step of the Hamiltonian dynamics
+        :param length: length of the Hamiltonian dynamics
+        :param momentum_std: standard deviation of the Gaussian distribution
+         the momentum is drawn from
+        """
 
         if not (self.init_obs and self.init_y and self.gp):
             raise NoExplorationError
 
-        self._update_current_sample('sampling')
+        self.init_y = np.min(self._list_y)
+        self.init_obs = self._list_obs[np.argmin(self._list_y)]
 
-        yield self.init_obs
+        while True:
+            self._update_current_sample(
+                'sampling', epsilon=epsilon, length=length,
+                momentum_std=momentum_std
+            )
+            yield self.init_obs
--- script/example_2d.py
+++ script/example_2d.py
@@ -1,13 +1,19 @@
 """GPHMC 2D example"""
+import matplotlib.pyplot as plt
 import numpy as np
+from tqdm import tqdm
 
-from gphmc.gphmc import GPHMCSampler
 from gaussian_process_regression.gaussian_process.covariance import (
     SquaredExponential
 )
+from gaussian_process_regression.gaussian_process.gp import GaussianProcess
 from gaussian_process_regression.gaussian_process.optimizer import (
     SECovLikelihoodOptimizer
 )
+from gaussian_process_regression.gaussian_process.util import get_logger
+from gphmc.gphmc import GPHMCSampler
+
+FUZZ = 1e-300
 
 
 def potential_energy(x):
@@ -17,9 +23,10 @@ def potential_energy(x):
     :return: value of the density at (x, y)
     """
 
-    probability = np.exp(-(1-(x[0]**2/2+x[1]**2/0.5))**2/0.5)
-    potential_energy = -np.log(probability)
-    return potential_energy
+    probability = np.exp(-8*(x[0]**2/2+x[1]**2-1)**2) + FUZZ
+    energy = -np.log(probability)
+
+    return energy
 
 
 def get_observations(xmin, xmax, ymin, ymax, n):
@@ -40,24 +47,124 @@ def get_observations(xmin, xmax, ymin, ymax, n):
         i = np.random.uniform(low=xmin, high=xmax)
         j = np.random.uniform(low=ymin, high=ymax)
         list_observations.append((i, j))
-        list_y.append(potential_energy(i, j))
+        list_y.append(potential_energy((i, j)))
 
     return list_observations, list_y
 
 
-def main():
+def plot_density(n=50):
+    """Plot the density
 
-    list_obs, list_y = get_observations(
-        xmin=-3, xmax=3, ymin=-3, ymax=3, n=15
+    :param n: number of point on each axis to estimate the density on.
+     The density will be estimated on a regular n x n grid n the [-2, 2, -2, 2]
+     space.
+    """
+
+    list_x = np.linspace(-2, 2, n)
+    coords = np.meshgrid(list_x, list_x, indexing='ij')
+    density_value = np.exp(-potential_energy([coords[0], coords[1]]))
+    plt.imshow(
+        density_value, cmap='viridis',
+        extent=[-2, 2, -2, 2], vmin=0.0, vmax=1.0, aspect='equal'
     )
-    GPHMCSampler(dimension=2, covariance_class=SquaredExponential, n_explo=100,
-                 epsilon=0.05, dynamics_length=20,
-                 likelihood_optimizer_class=SECovLikelihoodOptimizer,
-                 list_obs=list_obs, list_y=list_y,
-                 init_cov_param=np.array([1, 1, 1]),
-                 noise=1e-6, target_function=potential_energy)
+    plt.title('Real Density')
+    plt.savefig('figures/density.png', dpi=100, bbox_inches='tight')
+    plt.close()
 
 
-if __name__ == '__name__':
+def plot_gp(current_cov_param, list_obs, list_y, n=50):
+    """Plot the Gaussian process estimation of the density
+
+    :param current_cov_param: tuple with the 3 parameters of the exponential
+     covariance matrix
+    :param list_obs: list of 2-tuple corresponding to the point observed
+    :param list_y: list of scalar corresponding to the density value at the
+     observed points
+    :param n: number of point on each axis to estimate the density on.
+     The density will be estimated on a regular n x n grid n the [-2, 2, -2, 2]
+     space.
+    """
+
+    cov = SquaredExponential(current_cov_param[0], current_cov_param[1:])
+    gp = GaussianProcess(cov, list_observations=list_obs,
+                         list_y=list_y, noise=1e-3)
+    gp.covariance_matrix()
+    list_x = np.linspace(-2, 2, n)
+    current_estimation = np.zeros((n, n))
+    current_estimation_flat = [
+        (i, j, gp.mean([(xi, yj)])[0])
+        for (i, xi) in enumerate(list_x)
+        for (j, yj) in enumerate(list_x)
+    ]
+    for coord_value in current_estimation_flat:
+        current_estimation[coord_value[:2]] = np.exp(-coord_value[2])
+    plt.imshow(current_estimation, cmap='viridis', vmin=0, vmax=1,
+               extent=[-2, 2, -2, 2], aspect='equal')
+    idx_x = np.array([abs(x[0]) <= 1.8 for x in gp.list_observations])
+    idx_y = np.array([abs(x[1]) <= 1.8 for x in gp.list_observations])
+    idx = idx_x & idx_y
+    x = np.array([-x[0] for x in gp.list_observations])
+    y = np.array([x[1] for x in gp.list_observations])
+    z = np.array([np.exp(-y) for y in gp.list_y])
+    plt.scatter(
+        y[idx], x[idx], c=z[idx], cmap='viridis', s=100, vmin=0, vmax=1
+    )
+    plt.xlim(-2, 2)
+    plt.ylim(-2, 2)
+    plt.title('Gaussian process after exploration')
+    plt.savefig('figures/estimation.png', dpi=100, bbox_inches='tight')
+    plt.close()
+
+
+def plot_samples(samples):
+    """Plot the final samples
+
+    :param samples: list of tuple corresponding to the samples of the GPHMC
+    """
+
+    x = [-sample[0] for sample in samples]
+    y = [sample[1] for sample in samples]
+    plt.imshow(np.ones((50, 50)), cmap='gray', vmin=0, vmax=1,
+               extent=[-2, 2, -2, 2], aspect='equal')
+    plt.scatter(y, x, s=100, c='w')
+    plt.xlim(-2, 2)
+    plt.ylim(-2, 2)
+    plt.title('Final samples')
+    plt.savefig('figures/sample.png', dpi=100, bbox_inches='tight')
+    plt.close()
+
+
+def main():
+    """Main"""
+
+    np.random.seed(0)
+
+    plot_density()
+    logger = get_logger()
+    logger.info('Get initial observations')
+    list_obs, list_y = get_observations(xmin=-2, xmax=2, ymin=-2, ymax=2, n=50)
+    logger.info('Instantiate the sampler')
+    sampler = GPHMCSampler(
+        covariance_class=SquaredExponential, target_function=potential_energy,
+        likelihood_optimizer_class=SECovLikelihoodOptimizer,
+        list_obs=list_obs, list_y=list_y, noise=1e-3, dimension=2, n_explo=50,
+        init_cov_param=np.array([1, 1, 1])
+    )
+    logger.info('Exploration phase')
+    sampler.exploration(
+        epsilon=0.1, length=400, momentum_std=1.0, gp_update_rate=10
+    )
+    plot_gp(sampler.current_cov_param, sampler.list_obs, sampler.list_y)
+    logger.info('Sampling phase')
+    samples = []
+    sample_generator = sampler.sample(
+        epsilon=0.01, length=200, momentum_std=1.0
+    )
+    for _ in tqdm(range(500)):
+        samples.append(next(sample_generator))
+    plot_samples(samples)
+
+
+if __name__ == '__main__':
 
     main()
